9f236b05458548cc237d61e955061258c7cf4830
MuzykaAndrii <andrymyzik@gmail.com>
2024-11-27 00:31:40 +0200

    refactor: entrypoint via flask run command

--- app/app.py
+++ app/app.py
@@ -3,8 +3,21 @@ from flask import Flask
 from app.db.config import DbSettings
 from app.config import ENV_FILE_PATH
 from app.db.database import Database
+from app.users.dao import UserDAO
+from app.users.services import UserService
 
 
-app = Flask(__name__)
 db_settings = DbSettings(_env_file=ENV_FILE_PATH)
 db = Database(db_settings)
+
+users_dao = UserDAO(db.session_factory)
+user_service = UserService(users_dao)
+
+
+def create_app():
+    app = Flask(__name__)
+    from app.users.routes import router as users_router
+
+    app.register_blueprint(users_router)
+
+    return app
--- main.py
+++
@@ -1,4 +0,0 @@
-from app.app import app
-
-if __name__ == "__main__":
-    app.run("127.0.0.1", 8080, debug=True)
