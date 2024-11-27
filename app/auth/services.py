dac321a542bc58a30ff54f040506b25a5d538080
MuzykaAndrii <andrymyzik@gmail.com>
2024-11-27 16:25:19 +0200

    refactor: auth services

--- app/auth/services.py
+++ app/auth/services.py
@@ -1,13 +1,12 @@
 from functools import wraps
 from typing import Callable
 
-from flask import request, jsonify
+from flask import request, jsonify, Response
 
-from bookstore.auth.jwt import AuthTokenManager
-from bookstore.users.services import UserService
-from bookstore.users.dto import UserCreateDTO, UserLoginDTO, UserReadDTO
-from bookstore.response import JsonResponse
-from bookstore.auth.exceptions import AuthError
+from app.auth.jwt import JwtManager
+from app.users.services import UserService
+from app.users.dto import UserLoginDTO
+from app.auth.exceptions import AuthError
 
 
 class AuthService:
@@ -15,27 +14,20 @@ class AuthService:
 
     cookie_name: str = "auth_token"
 
-    def __init__(
-        self, token_handler: AuthTokenManager, user_service: UserService
-    ) -> None:
-        self._token_handler = token_handler
+    def __init__(self, jwt_manager: JwtManager, user_service: UserService) -> None:
+        self._jwt_manager = jwt_manager
         self._user_service = user_service
 
-    def register_user(self, credentials: UserCreateDTO) -> UserReadDTO:
-        """Registers a new user and returns user data."""
-
-        return self._user_service.create(credentials)
-
-    def login_user(self, credentials: UserLoginDTO) -> JsonResponse:
+    def login_user(self, credentials: UserLoginDTO) -> Response:
         """Authenticates a user and returns a response with an auth token cookie."""
 
         user = self._user_service.get_by_credentials(credentials)
-        token = self._token_handler.create_token(user.id)
+        token = self._jwt_manager.create_token(user.id)
 
         resp = jsonify({"token": token})
         resp.set_cookie(self.cookie_name, token)
 
-        return resp, 200
+        return resp
 
     def auth_required(self, router: Callable):
         """Decorator to protect routes, requiring a valid auth token to access."""
@@ -45,9 +37,9 @@ class AuthService:
             token = request.cookies.get(self.cookie_name)
 
             try:
-                self._token_handler.read_token(token)
+                self._jwt_manager.read_token(token)
             except AuthError:
-                return JsonResponse(status=401)
+                return jsonify(status=401)
             else:
                 return router(*args, **kwargs)
 
