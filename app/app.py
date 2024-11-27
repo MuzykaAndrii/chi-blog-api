fd79cbc3aa14612132e2f413b9fb6fd4619a3ff0
MuzykaAndrii <andrymyzik@gmail.com>
2024-11-27 16:36:58 +0200

    impl: auth setup

--- app/app.py
+++ app/app.py
@@ -1,5 +1,10 @@
+from datetime import timedelta
+
 from flask import Flask
 
+from app.auth.config import AuthSettings
+from app.auth.jwt import JwtManager
+from app.auth.services import AuthService
 from app.db.config import DbSettings
 from app.config import ENV_FILE_PATH
 from app.db.database import Database
@@ -13,11 +18,17 @@ db = Database(db_settings)
 users_dao = UserDAO(db.session_factory)
 user_service = UserService(users_dao)
 
+auth_settings = AuthSettings(_env_file=ENV_FILE_PATH)
+auth_jwt_manager = JwtManager("HS256", auth_settings.AUTH_SECRET, timedelta(days=1))
+auth_service = AuthService(auth_jwt_manager, user_service)
+
 
 def create_app():
     app = Flask(__name__)
     from app.users.routes import router as users_router
+    from app.auth.routes import router as auth_router
 
     app.register_blueprint(users_router)
+    app.register_blueprint(auth_router)
 
     return app
