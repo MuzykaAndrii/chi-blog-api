b1f68c175679b932cdb93639d9c03b84a7ecac14
MuzykaAndrii <andrymyzik@gmail.com>
2024-11-27 20:20:29 +0200

    impl: articles dao and services creation in setup

--- app/app.py
+++ app/app.py
@@ -2,6 +2,8 @@ from datetime import timedelta
 
 from flask import Flask
 
+from app.articles.dao import ArticleDAO
+from app.articles.services import ArticleService
 from app.auth.config import AuthSettings
 from app.auth.jwt import JwtManager
 from app.auth.services import AuthService
@@ -18,6 +20,9 @@ db = Database(db_settings)
 users_dao = UserDAO(db.session_factory)
 user_service = UserService(users_dao)
 
+articles_dao = ArticleDAO(db.session_factory)
+articles_service = ArticleService(articles_dao)
+
 auth_settings = AuthSettings(_env_file=ENV_FILE_PATH)
 auth_jwt_manager = JwtManager("HS256", auth_settings.AUTH_SECRET, timedelta(days=1))
 auth_service = AuthService(auth_jwt_manager, user_service)
@@ -27,9 +32,11 @@ def create_app():
     app = Flask(__name__)
     from app.users.routes import router as users_router
     from app.auth.routes import router as auth_router
+    from app.articles.routes import router as articles_router
 
     app.register_blueprint(users_router)
     app.register_blueprint(auth_router)
+    app.register_blueprint(articles_router)
 
     from app.error_handlers import register_error_handlers
 
---
+++ app/articles/routes.py
@@ -0,0 +1,4 @@
+from flask import Blueprint
+
+
+router = Blueprint("articles", __name__, url_prefix="/articles")
