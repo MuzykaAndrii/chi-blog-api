3a2fc74d5cd9eddae67b00f42cad157b1bf4d864
MuzykaAndrii <andrymyzik@gmail.com>
2024-12-01 19:18:47 +0200

    impl: swagger docs fot auth

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,13 +1,16 @@
 from flask import request, Blueprint
+from flasgger import swag_from
 
 from app.app import auth_service
 from app.utils.response import JsonResponse
 from app.users.exceptions import InvalidPassword
+from app.auth.swagger import docs
 
 router = Blueprint("auth", __name__, url_prefix="/auth")
 
 
 @router.post("/login")
+@swag_from(docs.LOGIN_USER)
 def login():
     """Logs in a user and returns auth token if credentials are valid."""
 
---
+++ app/auth/swagger/docs.py
@@ -0,0 +1,42 @@
+LOGIN_USER = {
+    "tags": ["Authentication"],
+    "description": "Logs in a user and returns a JWT token in the response.",
+    "parameters": [
+        {
+            "name": "body",
+            "in": "body",
+            "description": "User credentials (e.g., email and password)",
+            "required": True,
+            "schema": {
+                "type": "object",
+                "properties": {
+                    "email": {
+                        "type": "string",
+                        "description": "The email of the user",
+                    },
+                    "password": {
+                        "type": "string",
+                        "description": "The password of the user",
+                    },
+                },
+                "required": ["email", "password"],
+            },
+        }
+    ],
+    "responses": {
+        "200": {
+            "description": "Login successful, returns JWT token",
+            "content": {
+                "application/json": {
+                    "example": {
+                        "token": "jwt_token_here",
+                    }
+                }
+            },
+        },
+        "401": {
+            "description": "Invalid credentials or authentication error",
+            "content": {"application/json": {"example": {"error": "Invalid password"}}},
+        },
+    },
+}
