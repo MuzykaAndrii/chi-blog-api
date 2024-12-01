b87b5f089093c324d4ea50317e47b2e8e8e12ab2
MuzykaAndrii <andrymyzik@gmail.com>
2024-12-01 02:44:19 +0200

    impl: get permission endpoint

--- app/rbac/routes/permission.py
+++ app/rbac/routes/permission.py
@@ -11,3 +11,9 @@ router = Blueprint("permissions", __name__, url_prefix="/permissions")
 def get_all_permissions():
     permissions = permission_service.get_all_permissions()
     return JsonResponse(permissions.model_dump_json(), status=200)
+
+
+@router.get("/<int:permission_id>")
+def get_permission(permission_id: int):
+    permission = permission_service.get_permission_by_id(permission_id)
+    return JsonResponse(permission.model_dump_json(), status=200)
--- app/rbac/services/permission.py
+++ app/rbac/services/permission.py
@@ -1,5 +1,6 @@
 from app.rbac.dao.permission import PermissionDAO
-from app.rbac.dto import PermissionsListReadDTO
+from app.rbac.dto import PermissionReadDTO, PermissionsListReadDTO
+from app.rbac.exceptions import PermissionNotFound
 
 
 class PermissionService:
@@ -9,3 +10,11 @@ class PermissionService:
     def get_all_permissions(self) -> PermissionsListReadDTO:
         permissions = self._permission_dao.get_all()
         return PermissionsListReadDTO.model_validate(permissions)
+
+    def get_permission_by_id(self, permission_id: int) -> PermissionReadDTO:
+        permission = self._permission_dao.get_one(permission_id)
+
+        if not permission:
+            raise PermissionNotFound
+
+        return PermissionReadDTO.model_validate(permission)
