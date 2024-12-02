6a2e81aa1beca9f65ccddf5791b3226693ee52d0
MuzykaAndrii <andrymyzik@gmail.com>
2024-12-03 00:40:43 +0200

    unit test: test_get_users_list_without_search

--- tests/unit_tests/users/test_user_routes.py
+++ tests/unit_tests/users/test_user_routes.py
@@ -34,6 +34,19 @@ def client(app: Flask) -> FlaskClient:
 
 
 @patch("app.users.routes.user_service")
+def test_get_users_list_without_search(user_service: UserService, client: FlaskClient):
+    mock_users = [{"id": 1, "name": "user1"}, {"id": 2, "name": "user2"}]
+    mock_users_json = json.dumps(mock_users)
+
+    user_service.get_all_users.return_value = mock_users_json
+
+    response = client.get("/users")
+
+    assert response.status_code == 200
+    assert response.json == mock_users
+
+
+@patch("app.users.routes.user_service")
 def test_get_users_list_with_search(user_service: UserService, client: FlaskClient):
     search_q = "andry"
 
