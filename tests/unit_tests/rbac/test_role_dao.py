31eba93742b2840f6d086b920123704df5950946
MuzykaAndrii <andrymyzik@gmail.com>
2024-12-02 21:53:56 +0200

    unit test: test_get_all_roles

--- tests/unit_tests/rbac/test_role_dao.py
+++ tests/unit_tests/rbac/test_role_dao.py
@@ -31,3 +31,17 @@ def test_get_role_by_id(role_dao: RoleDAO, mock_role: MagicMock):
     assert result.id == 1
     assert result.name == "admin"
     mock_session.get.assert_called_once_with(Role, 1)
+
+
+def test_get_all_roles(role_dao: RoleDAO, mock_role: MagicMock):
+    mock_session = MagicMock()
+    role_dao._sf.return_value.__enter__.return_value = mock_session
+    mock_session.scalars.return_value.all.return_value = [mock_role, mock_role]
+
+    result = role_dao.get_all()
+
+    assert len(result) == 2
+    assert result[0].name == mock_role.name
+    assert result[1].name == mock_role.name
+
+    mock_session.scalars.assert_called_once()
